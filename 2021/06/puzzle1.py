e8bb884479e44a498f4177e75b82a4d243a50575
liam <liamtrinalizo61@gmail.com>
2021-12-06 19:02:46 +0100

    solve day 6 part 1

---
+++ 2021/06/puzzle1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+days=80
+
+with open("input") as file:
+    fishes = [int(i) for i in file.readline().split(sep=",")]
+    for i in range(days):
+        for fish_index in range(len(fishes) - 1, -1, -1):
+            if fishes[fish_index] == 0:
+                fishes.append(8)
+                fishes[fish_index] = 6
+            else:
+                fishes[fish_index] -= 1
+        #print("{:<2}".format(days), fishes)
+    print("Number of fish:", len(fishes))
