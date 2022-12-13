eef6716c0b1367355cbb00480bf549ffa9a68290
liam <liamtrinalizo61@gmail.com>
2022-12-13 14:14:23 +0100

    add visualization to 2022 day 7

---
+++ 2022/07/visu.cpp
@@ -0,0 +1,113 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+struct Dir {
+    int size;
+    string name;
+    map<string, Dir *> subDir;
+};
+
+int main() {
+    ifstream file("input");
+    string line;
+
+    Dir *curr, *prev, *root = new Dir();
+    curr = root;
+    curr->subDir[".."] = root;
+    curr->name = "/";
+    curr->size = 0;
+
+    puts("digraph {\n"
+         "    rankdir=LR\n"
+         "    overlap=false\n"
+         "    labelloc=\"t\"\n"
+         "    fontsize=\"100pt\"\n"
+         "    label=\"File system tree (label font size according to file size)\"\n"
+         //"    layout=neato\n"
+         //"    graph [splines=ortho]\n"
+         //"    node [shape=plaintext]\n"
+         "    \"///\" [label=\"/\"]\n");
+
+    // skip first line going to root Dir
+    getline(file, line);
+    while (getline(file, line)) {
+        string cmd = line.substr(0, 4);
+        if (cmd == "$ cd") {
+            // change current with new dir and save previous position
+            string dirname = line.substr(5, line.size()-1);
+            prev = curr;
+            curr = curr->subDir[dirname];
+
+            // save parent only in child dir
+
+            if (dirname != "..") {
+                curr->subDir[".."] = prev;
+                curr->size = 0;
+                printf("\"%s%s%s\" [label=\"%s\"]\n", curr->subDir[".."]->subDir[".."]->name.c_str(),
+                                                      curr->subDir[".."]->name.c_str(),
+                                                      curr->name.c_str(),
+                                                      curr->name.c_str());
+            } else
+                curr->size += prev->size;
+        }
+        else if (cmd != "$ ls") {
+            istringstream stream(line);
+            int size = 0;
+            string filename;
+            stream >> filename;
+            if (stringstream(filename) >> size) {
+                curr->size += size;
+                stream >> filename;
+            } else {
+                stream >> filename;
+                curr->subDir[filename] = new Dir;
+                curr->subDir[filename]->name = filename;
+            }
+            // combine grand-parent, parent and child name to avoid conflict
+            printf("\"%s%s%s\" [label=\"%s\" fontsize=\"%dpt\"]\n", curr->subDir[".."]->name.c_str(),
+                                                  curr->name.c_str(),
+                                                  filename.c_str(),
+                                                  filename.c_str(),
+                                                  size ? size/5000 : 10);
+            printf("\"%s%s%s\" -> \"%s%s%s\"\n", curr->subDir[".."]->subDir[".."]->name.c_str(),
+                                                 curr->subDir[".."]->name.c_str(),
+                                                 curr->name.c_str(),
+                                                 curr->subDir[".."]->name.c_str(),
+                                                 curr->name.c_str(),
+                                                 filename.c_str());
+
+        }
+    }
+    // go back to root while adding sub dir size
+    while (curr != root) {
+        prev = curr;
+        curr = curr->subDir[".."];
+        curr->size += prev->size;
+    }
+
+    // traverse dirs
+    int tot = 0;
+    curr = root;
+    stack<Dir *> dirs;
+    for (auto &d : curr->subDir) {
+        if (d.first != "..")
+            dirs.push(d.second);
+    }
+    while (!dirs.empty()) {
+        curr = dirs.top();
+        dirs.pop();
+        if (curr->size <= 100000)
+            tot += curr->size;
+        for (auto &d : curr->subDir) {
+            if (d.first != "..")
+                dirs.push(d.second);
+        }
+    }
+    cout << "}";
+}
